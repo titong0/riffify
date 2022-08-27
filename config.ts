3515b6cd613c93054d172c24a4746991fb8694f1
Titong0 <augustoerrecarte@gmail.com>
2022-08-26 23:04:35 -0300

    add password to config

--- config.ts
+++ config.ts
@@ -1,1 +1,2 @@
 export const api = process.env.API || "http://localhost:4000";
+export const password = process.env.PASSWORD || null;
--- service.ts
+++ service.ts
@@ -1,8 +1,9 @@
-import { api } from "./config";
+import { api, password } from "./config";
 import { ArtistFromSearch } from "./types";
 
 export const getArtistsQuery = async (artist: string) => {
   const params = new URLSearchParams({ query: artist });
+  password && params.append("password", password);
   const data = await fetch(`${api}/search?${params}`);
 
   if (data.status === 400) return null;
