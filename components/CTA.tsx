62722f1d3bcdaabff3f5d9c9806ac82bfc25e5c3
tito-depto <augustoerrecarte@gmail.com>
2023-02-09 15:50:04 -0300

    less rounding

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -8,7 +8,7 @@ const CTA = ({ ...props }: CTAProps) => {
   return (
     <button
       {...props}
-      className="w-full p-2 px-8 ml-auto text-white transition border border-black rounded-2xl bg-emerald-500 hover:outline outline-offset-2 outline-black active:bg-emerald-800"
+      className="w-full p-2 px-8 ml-auto text-white transition border border-black rounded-xl bg-emerald-500 hover:outline outline-offset-2 outline-black active:bg-emerald-800"
     >
       {props.children}
     </button>
