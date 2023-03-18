a0750669d9a505e51d644b176ef45ff507d346d3
Titong0 <augustoerrecarte@gmail.com>
2023-03-17 23:49:44 -0300

    redesign song snippet

--- components/SongSnippet.tsx
+++ components/SongSnippet.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import { Game_Song } from "../shared/schemas";
 
@@ -9,14 +10,32 @@ export default function SongSnippet({
   failAmount: number;
 }) {
   return (
-    <div className="flex flex-col items-center gap-2 py-4 m-2 border w-fit">
+    <div className="flex flex-col items-center gap-2 py-4 m-2 w-fit">
       <h3 className="">
         You guessed the song in <span className="font-bold">{failAmount} </span>
         {failAmount === 1 ? "attempt" : "attempts"}
       </h3>
-      <p>{song.title}</p>
-      <a href={`https://www.youtube.com/watch?v=${song.id}`} className="w-1/3">
-        <img referrerPolicy={"no-referrer"} src={song.album.thumbnail} alt="" />
+      <a
+        href={`https://www.youtube.com/watch?v=${song.id}`}
+        className="w-full sm:bg-blue-500"
+      >
+        <div className="grid w-full  sm:grid-cols-6 gap-2 min-h-[100px]">
+          <div className="w-full min-w-[100px]">
+            <Image
+              unoptimized
+              layout="responsive"
+              width={"544"}
+              height={"544"}
+              referrerPolicy={"no-referrer"}
+              src={song.album.thumbnail}
+              alt=""
+            />
+          </div>
+          <div className="col-start-2 p-2 col-span-full sm:col-end-5">
+            <h2 className="text-xl font-bold">{song.title}</h2>
+            <p>{song.album.name}</p>
+          </div>
+        </div>
       </a>
     </div>
   );
--- components/WinningScreen.tsx
+++ components/WinningScreen.tsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 import { Game_Song } from "../shared/schemas";
 import CTA from "./common/CTA";
 import SongSnippet from "./SongSnippet";
-import ArtistStats from "./UserStats";
+import Stats from "./ArtistStats";
+import AlertDialogWrapper from "./common/AlertDialogWrapper";
 
 type WinningScreenProps = {
   artistId: string;
@@ -14,37 +15,26 @@ const WinningScreen = ({ song, failAmount }: WinningScreenProps) => {
   const [open, setOpen] = useState(true);
 
   return (
-    <div className="">
-      <button
-        className={`rounded-lg p-3 ${open ? "bg-red-400" : "bg-emerald-500"}`}
-        onClick={() => setOpen(!open)}
-      >
-        Trigger
-      </button>
-      <AlertDialog.Root open={open} onOpenChange={(e) => setOpen(e)}>
-        <AlertDialog.Trigger />
-        <AlertDialog.Portal>
-          <AlertDialog.Overlay className="bg-black bg-opacity-80 animate-[fade-in_300ms] fixed inset-0 z-40" />
-          <AlertDialog.Content>
-            <div
-              className="absolute top-16 left-1/2 w-[95%] md:w-4/5 max-w-2xl min-h-72 bg-gray-900 rounded-md -translate-x-1/2
-            p-2 animate-[fade-in_600ms] animate-[] z-50 flex flex-col items-center"
-            >
-              <AlertDialog.Title className="my-2 text-2xl font-bold text-center">
-                {" "}
-                You won!
-              </AlertDialog.Title>
-              {/* <ArtistStats artistId={artistId} /> */}
-              <SongSnippet failAmount={failAmount} song={song} />
+    <AlertDialog.Root open={open} onOpenChange={(e) => setOpen(e)}>
+      <AlertDialog.Trigger />
+      <AlertDialog.Portal>
+        <AlertDialog.Overlay className="bg-black bg-opacity-80 animate-[fade-in_300ms] fixed inset-0 z-40" />
+        <AlertDialog.Content>
+          <AlertDialogWrapper>
+            <AlertDialog.Title className="my-2 text-2xl font-bold text-center">
+              {" "}
+              You won!
+            </AlertDialog.Title>
+            {/* <ArtistStats artistId={artistId} /> */}
+            <SongSnippet failAmount={failAmount} song={song} />
 
-              <AlertDialog.Cancel asChild className="w-full">
-                <CTA className="w-full">Cool</CTA>
-              </AlertDialog.Cancel>
-            </div>
-          </AlertDialog.Content>
-        </AlertDialog.Portal>
-      </AlertDialog.Root>
-    </div>
+            <AlertDialog.Cancel asChild className="w-full">
+              <CTA className="w-full">Cool</CTA>
+            </AlertDialog.Cancel>
+          </AlertDialogWrapper>
+        </AlertDialog.Content>
+      </AlertDialog.Portal>
+    </AlertDialog.Root>
   );
 };
 
