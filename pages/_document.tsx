8070e8a6e1a06ad2a07e248c883b549ab2efd7dc
Titong0 <augustoerrecarte@gmail.com>
2023-04-15 01:15:46 -0300

    prettier things

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,8 +16,34 @@ class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@400;700;500&display=swap"
             rel="stylesheet"
           />
+          {/* generated with https://realfavicongenerator.net  */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#00aba9" />
+          <meta
+            name="msapplication-config"
+            content="/favicons/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
-        <body className="min-h-screen font-libre bg-gradient-to-b dark:from-purplish dark:to-cyan-800 dark:bg-neutral-900 dark:text-gray-100">
+        <body className="min-h-screen font-libre bg-gradient-to-b dark:from-purplish dark:to-cyan-800 dark:bg-neutral-900 dark:text-gray-100 -z-10">
           <Main />
           <NextScript />
         </body>
