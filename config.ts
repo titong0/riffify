3ebba0315cc5c386c3784747abbdecc643b369ec
Titong0 <augustoerrecarte@gmail.com>
2023-04-15 01:18:17 -0300

    update domain

--- config.ts
+++ config.ts
@@ -2,5 +2,5 @@ export const api = process.env.API || "http://localhost:4000";
 export const host =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://auto-heardle-frontend.vercel.app";
+    : "https://riffify.vercel.app";
 export const password = process.env.PASSWORD || null;
