db3a6677d6c52175744a940cc2bc895275f86d1a
Titong0 <augustoerrecarte@gmail.com>
2023-04-30 13:34:04 -0300

    GOD DAMN WHY DID I DO THIS LIKE THAT IM SO STUPID

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -1,8 +1,17 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { host } from "../../config";
-async function callDeleteUpdatedPages() {
-  const res = await fetch(
-    `${host}/api/deleteUpdatedPages?key=${process.env.ADMIN_KEY}`
-  );
-  return res.json();
+
+async function callDeleteUpdatedPages(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const data = await fetch(
+      `${host}/api/deleteUpdatedPages?key=${process.env.ADMIN_KEY}`
+    );
+    return res.json(await data.json());
+  } catch (error) {
+    res.status(500).json(error);
+  }
 }
 export default callDeleteUpdatedPages;
