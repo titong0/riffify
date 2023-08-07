f304beaaf6b6b1eb140711ad4312f8b01b6a6bf6
tito-depto <augustoerrecarte@gmail.com>
2023-08-07 09:44:10 -0300

    add message idk why it looks cool

--- components/common/HeardleBeingUpdated.tsx
+++ components/common/HeardleBeingUpdated.tsx
@@ -1,8 +1,13 @@
 import React, { useCallback } from "react";
-import { host } from "../../config";
 import { useRouter } from "next/router";
 
-const HeardleBeingUpdated = ({ artistId }: { artistId: string }) => {
+const HeardleBeingUpdated = ({
+  artistId,
+  songAmount,
+}: {
+  artistId: string;
+  songAmount: number;
+}) => {
   const Router = useRouter();
   setTimeout(() => {
     Router.reload();
@@ -11,7 +16,10 @@ const HeardleBeingUpdated = ({ artistId }: { artistId: string }) => {
     <div className="flex flex-col items-center justify-center gap-12 h-80">
       <div className="w-full max-w-2xl p-3 font-semibold text-gray-900 bg-amber-300 ">
         You are the first person to play this heardle today. Please stand by
-        while it is updated. This shouldn&apos;t take longer than 5 seconds...
+        while it is updated. This artist has {songAmount} songs, so
+        {songAmount > 100
+          ? `it shouldn't take longer than 5 seconds...`
+          : "it may take a bit..."}
       </div>
       <div className="animate-bounce">Just wait, please</div>
       <div className="animate-[spin_5s_infinite]">Just wait, please</div>
